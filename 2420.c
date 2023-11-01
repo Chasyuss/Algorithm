40b8b1d0fe814930a46078189fbb7ed9d3d75516
chasyuss <cotn963@gmail.com>
2023-11-01 21:55:23 +0900

    백준 문제 2420번

---
+++ 2420.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    long long N,M; 
+    scanf("%lld %lld", &N, &M);
+
+    if( N < M)
+    {
+        printf("%lld", M-N);
+    }
+
+    else{
+        printf("%lld", N-M);
+    }
+}   
