0ce798130ab3b66c107d72de446b04619aa63285
chasyuss <cotn963@gmail.com>
2023-11-11 02:33:22 +0900

    2752번

---
+++ 2752.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int arr[1000000];
+    int tmp; 
+
+    for(int i =0; i<3; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    for(int i = 0; i<3; i++)
+    {
+        for(int k= 0; k<2-i; k++)
+        {
+            if(arr[k] > arr[k+1])
+            {
+                tmp = arr[k];
+                arr[k] = arr[k+1];
+                arr[k+1] = tmp;
+            
+            }
+        }
+    }
+    for(int i=0;i<3;i++)
+    {
+        printf("%d ", arr[i]); 
+    }
+    return 0; 
+
+}
