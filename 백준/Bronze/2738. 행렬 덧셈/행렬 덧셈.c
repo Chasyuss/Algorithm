070189c3c68e6e12a125bc85c3fe12cff13da549
ChaeSu <127167163+Chasyuss@users.noreply.github.com>
2024-04-17 11:59:46 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int n, m;
+    int arr1[101][101];
+    int arr2[101][101];
+
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < m; k++)
+        {
+            scanf("%d", &arr1[i][k]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < m; k++)
+        {
+            scanf("%d", &arr2[i][k]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < m; k++)
+        {
+            printf("%d ", arr1[i][k] + arr2[i][k]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
