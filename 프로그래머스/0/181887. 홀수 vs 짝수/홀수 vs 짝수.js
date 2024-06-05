73248f633ebbf78d89812a39f1c4460728ef8024
ChaeSu <127167163+Chasyuss@users.noreply.github.com>
2024-06-05 09:32:04 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,20 @@
+function solution(num_list) {
+    let odd = 0; 
+    let even = 0; 
+    
+    for(let i =0; i<num_list.length; i++){
+        if(i%2 === 0)
+            {
+                odd += num_list[i];
+            }
+        else{
+            even +=num_list[i];
+        }
+    }
+    if(odd>even){
+        return odd; 
+    }
+    else{
+        return even; 
+    }
+}
