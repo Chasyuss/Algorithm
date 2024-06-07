60cce99a55a18f32b78df6919d7826a97f54e2ec
ChaeSu <127167163+Chasyuss@users.noreply.github.com>
2024-06-07 15:46:36 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,8 +1,9 @@
 function solution(my_string) {
   let answer = [];
   for (let i of my_string) {
-    if (i.charCodeAt() < 58) 
-        answer.push(Number(i));
+    if (!isNaN(i) && i !== "") {
+      answer.push(Number(i));
+    }
   }
-  return answer.sort();
+  return answer.sort((a, b) => a - b);
 }
