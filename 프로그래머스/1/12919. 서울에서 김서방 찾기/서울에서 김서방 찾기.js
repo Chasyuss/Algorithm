f0cabfa9ef7f03d5424c5fbddea84530a7cf3a62
ChaeSu <127167163+Chasyuss@users.noreply.github.com>
2024-06-17 09:15:49 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,6 +1,11 @@
+// function solution(seoul) {
+//     for (let i = 0; i < seoul.length; i++) {
+//         if (seoul[i] === 'Kim') 
+//             return `김서방은 ${i}에 있다`
+//     }
+// }
+
 function solution(seoul) {
-    for (let i = 0; i < seoul.length; i++) {
-        if (seoul[i] === 'Kim') 
-            return `김서방은 ${i}에 있다`
-    }
+    let location = seoul.indexOf('Kim')
+    return `김서방은 ${location}에 있다`;
 }
