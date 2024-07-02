057bb5097f49ff1ab41b1cb37a000e7af3691df6
ChaeSu <127167163+Chasyuss@users.noreply.github.com>
2024-07-02 09:06:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,11 +1,15 @@
 function solution(phone_number) {
-  var answer = "";
-  for (let i = 0; i < phone_number.length; i++) {
-    if (i > phone_number.length - 5) {
-      answer += phone_number[i];
-    } else {
-      answer += "*";
-    }
-  }
-  return answer;
+    var answer = '';
+    answer = '*'.repeat(phone_number.length-4) + phone_number.slice(-4);
+    return answer;
 }
+
+
+// for (let i = 0; i < phone_number.length; i++) {
+  //   if (i > phone_number.length - 5) {
+  //     answer += phone_number[i];
+  //   } else {
+  //     answer += "*";
+  //   }
+  // }
+  // return answer;
